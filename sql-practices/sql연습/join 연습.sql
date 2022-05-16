aae0bb2409361c9247cd3f2bf7824d2f534c3886
chaewon <rlacodnjs98@naver.com>
2022-05-16 11:27:05 +0900

    join 연습 마무리

--- sql-practices/sql연습/join 연습.sql
+++ sql-practices/sql연습/join 연습.sql
@@ -47,3 +47,58 @@ where a.to_date = '9999-01-01'
 	and b.to_date = '9999-01-01'
 group by b.title
 order by avg(a.salary) desc;
+
+-- 실습문제1 : 현재 회사 상황을 반형한 직원별 근무부서를 사번, 직원 전체이름, 근무부서 형태로 출력해 보세요.
+select  a.emp_no, a.first_name, b.dept_name
+	from employees a, departments b, dept_emp c
+where a .emp_no = c.emp_no 
+	and b.dept_no = c.dept_no
+	and c.to_date = '9999-01-01';
+
+--  실습문제 2:  현재 회사에서 지급되고 있는 급여체계를 반영한 결과를 출력하세요. 
+-- 사번,  전체이름, 연봉  이런 형태로 출력하세요.    
+select a.emp_no as '사번', a.first_name as '이름', b.salary as '연봉'
+	from employees a, salaries b
+where a.emp_no = b.emp_no
+	and b.to_date = '9999-01-01'
+order by b.salary desc;
+
+-- 실습문제3 : 현재 직책별로 평균 연봉과 인원수를 구하되 직책별로 인원이 100명 이상인 직책만 출력하세요.
+select a.title, avg(b.salary), count(*)
+	from titles a, salaries b
+where a.emp_no = b.emp_no
+	and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+group by a.title
+	having count(*) >= 100
+order by avg(b.salary) desc;
+
+-- 실습문제4 : 현재 부서별로 현재 직책이 Engineer인 직원들의 대해서만 평균 급여를 구하세요.
+select b.dept_no as '부서', c.title as '직책', a.first_name as '이름', avg(d.salary) as '평균급여' 
+	from employees a, dept_emp b, titles c, salaries d
+where a.emp_no = b.emp_no 
+	and a.emp_no = c.emp_no
+    and a.emp_no  = d.emp_no
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01'
+    and d.to_date = '9999-01-01'
+    and c.title = 'engineer'
+group by b.dept_no;
+    
+
+select d.dept_name, avg(salary)
+	from dept_emp a, salaries b, titles c, departments d
+where a.emp_no = b.emp_no
+	and b.emp_no = c.emp_no
+    and d.dept_no = a. dept_no
+    and a.to_date = '9999-01-01'
+    and b.to_date = '9999-01-01'
+    and c.to_date = '9999-01-01'
+    and c.title = 'engineer'
+group by a.dept_no;
+
+
+
+
+
+
